4f817bbc321ee7ae34e06de87c2a2a7898a6ed83
owen-d <ow.diehl@gmail.com>
2014-11-19 17:37:26 -0800

    serving html/css

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,36 @@
 var http = require("http");
 var handler = require("./request-handler");
+var url = require('url');
+var path = require('path');
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+var router = {
+  //routes pages
+  '/sites': handler.handleRequest
+};
+var whiteList = {
+  '/styles.css': handler.handleRequest,
+  '/': handler.handleRequest
+}
+
+var server = http.createServer( function(req, res){
+  var reqPath = url.parse(req.url).pathname;
+  // if(req.method === 'POST'){
+  //   console.log('post');
+  // }
+  console.log(req.url);
+  if(router[reqPath]){
+    router[reqPath](req, res, reqPath);
+  }
+  else if (whiteList[reqPath]){
+    whiteList[reqPath](req, res, reqPath);
+  } else {
+    res.writeHead(404, 'invalid request');
+    res.end("404");
+  }
+});
+
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,37 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
+var fs = require('fs');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var typeConversions = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript'
 };
+
+exports.handleRequest = function (req, res, reqPath) {
+  if (reqPath === '/') {
+    reqPath = './public/index.html';
+  }
+  else if (reqPath === '/styles.css') {
+    reqPath = './public/styles.css';
+  }
+  console.log(req.url);
+  fs.readFile(reqPath,{encoding:"utf8"}, function(err, data){
+    if(err){
+      res.writeHead(404, helpers.headers );
+      res.end(err);
+    } else{
+      helpers.headers['Content-Type'] = typeConversions[path.extname(reqPath)];
+      res.writeHead(200, helpers.headers)
+      res.write(data);
+      res.end();
+    }
+  });
+  // res.end(archive.paths.list);
+};
+
+
+//var headers = helpers.headers;
+//headers['content-type'] = require('url').
